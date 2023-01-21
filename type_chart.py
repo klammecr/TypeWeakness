13f543ca7384229a845ed7e46136833e7edf6b5b
M3rrowY3rrow <Joseph.raven1234@yahoo.com>
2023-01-20 23:51:29 -0500

    type weaknesss chart

--- type_chart.py
+++ type_chart.py
@@ -22,9 +22,20 @@ class PokemonType(Enum):
 
 print(f'number of valid types:{len(valid_pokemon_type)}')
 weak_table = {PokemonType.BUG:[PokemonType.FIRE, PokemonType.FLYING,PokemonType.ROCK],
-              PokemonType.DARK:[PokemonType.BUG, PokemonType.FAIRY, PokemonType.ICE], 
+              PokemonType.DARK:[PokemonType.BUG, PokemonType.FAIRY, PokemonType.FIGHTING], 
               PokemonType.DRAGON:[PokemonType.DRAGON, PokemonType.FAIRY, PokemonType.ICE],
-              PokemonType.ELECTRIC
-              PokemonType.FIRE:[PokemonType.GROUND, PokemonType.ROCK, PokemonType.WATER],
-              PokemonType.WATER:[PokemonType.ELECTRIC,PokemonType.GRASS], 
-              PokemonType.GRASS:['bug','fire',]}
+              PokemonType.ELECTRIC:[PokemonType.GROUND], 
+              PokemonType.FAIRY:[PokemonType.POISON, PokemonType.STEEL], 
+              PokemonType.FIGHTING:[PokemonType.FAIRY, PokemonType.FLYING, PokemonType.PSYCHIC], 
+              PokemonType.FLYING:[PokemonType.ELECTRIC, PokemonType.ICE, PokemonType.ROCK], 
+              PokemonType.GHOST:[PokemonType.DARK, PokemonType.GHOST], 
+              PokemonType.GROUND:[PokemonType.GRASS, PokemonType.ICE, PokemonType.WATER], 
+              PokemonType.ICE:[PokemonType.FIGHTING, PokemonType.FIRE, PokemonType.ROCK, PokemonType.STEEL],
+              PokemonType.NORMAL:[PokemonType.FIGHTING], 
+              PokemonType.POISON:[PokemonType.GROUND, PokemonType.PSYCHIC], 
+              PokemonType.PSYCHIC:[PokemonType.BUG, PokemonType.DARK, PokemonType.GHOST], 
+              PokemonType.ROCK:[PokemonType.FIGHTING, PokemonType.GRASS, PokemonType.GROUND, PokemonType.STEEL, PokemonType.WATER], 
+              PokemonType.STEEL:[PokemonType.FIGHTING, PokemonType.FIRE, PokemonType.GROUND], 
+              PokemonType.FIRE:[PokemonType.GROUND, PokemonType.ROCK, PokemonType.WATER], 
+              PokemonType.WATER:[PokemonType.ELECTRIC, PokemonType.GRASS], 
+              PokemonType.GRASS:[PokemonType.BUG, PokemonType.FIRE, PokemonType.FLYING, PokemonType.ICE, PokemonType.POISON]}
