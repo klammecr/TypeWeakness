d5c9809f628dfb3a3433b98d90d5534764d1094f
M3rrowY3rrow <Joseph.raven1234@yahoo.com>
2023-01-20 23:24:48 -0500

    begin type table

---
+++ type_chart.py
@@ -0,0 +1,3 @@
+#pokemon_type = ""
+valid_pokemon_type = ['Fire', 'Water', 'Grass', 'Electric', 'Ice', 'Ground', 'Rock', 'Fairy', 'Dragon', 'Dark', 'Ghost', 'Psychic', 'Bug', 'Steel', 'Poison', 'Normal', 'Fighting', 'Flying']
+print(f'number of valid types:{len(valid_pokemon_type)}')
