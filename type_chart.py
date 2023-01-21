da99b19bbd1abf520935e427481522fd874d84f0
klammecr <christopher.klammer@gmail.com>
2023-01-21 04:39:25 -0500

    Strong table

--- type_chart.py
+++ type_chart.py
@@ -28,3 +28,25 @@ weak_table = {PokemonType.BUG:[PokemonType.FIRE, PokemonType.FLYING,PokemonType.
               PokemonType.FIRE:[PokemonType.GROUND, PokemonType.ROCK, PokemonType.WATER],
               PokemonType.WATER:[PokemonType.ELECTRIC,PokemonType.GRASS], 
               PokemonType.GRASS:['bug','fire',]}
+
+strong_table = \
+{
+    PokemonType.BUG : [PokemonType.GRASS, PokemonType.DARK, PokemonType.PSYCHIC],        
+    PokemonType.DARK : [PokemonType.GHOST, PokemonType.PSYCHIC],
+    PokemonType.DRAGON : [PokemonType.DRAGON],
+    PokemonType.ELECTRIC : [PokemonType.FLYING, PokemonType.WATER],
+    PokemonType.FAIRY : [PokemonType.FIGHTING, PokemonType.DARK, PokemonType.DRAGON],
+    PokemonType.FIGHTING : [PokemonType.DARK, PokemonType.ICE, PokemonType.NORMAL, PokemonType.ROCK, PokemonType.STEEL],
+    PokemonType.FIRE : [PokemonType.BUG, PokemonType.GRASS, PokemonType.ICE, PokemonType.STEEL],
+    PokemonType.FLYING : [PokemonType.BUG, PokemonType.FIGHTING, PokemonType.GRASS],
+    PokemonType.GHOST : [PokemonType.GHOST, PokemonType.PSYCHIC],
+    PokemonType.GRASS : [PokemonType.GROUND, PokemonType.ROCK, PokemonType.WATER],
+    PokemonType.GROUND : [PokemonType.ELECTRIC, PokemonType.FIRE, PokemonType.POISON, PokemonType.ROCK, PokemonType.STEEL],
+    PokemonType.ICE : [PokemonType.DRAGON, PokemonType.FLYING, PokemonType.GRASS, PokemonType.GROUND],
+    PokemonType.NORMAL : [],
+    PokemonType.POISON : [PokemonType.FAIRY, PokemonType.GRASS],
+    PokemonType.PSYCHIC : [PokemonType.FIGHTING, PokemonType.POISON],
+    PokemonType.ROCK : [PokemonType.BUG, PokemonType.FIRE, PokemonType.FLYING, PokemonType.ICE],
+    PokemonType.STEEL : [PokemonType.FAIRY, PokemonType.ICE, PokemonType.ROCK],
+    PokemonType.WATER : [PokemonType.FIRE, PokemonType.GROUND, PokemonType.ROCK]      
+}
