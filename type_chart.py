3caf795ebdeab00e33a7036069989a873e526f2e
M3rrowY3rrow <Joseph.raven1234@yahoo.com>
2023-01-20 23:26:43 -0500

    begin type table

--- type_chart.py
+++ type_chart.py
@@ -1,3 +1,30 @@
 #pokemon_type = ""
-valid_pokemon_type = ['Fire', 'Water', 'Grass', 'Electric', 'Ice', 'Ground', 'Rock', 'Fairy', 'Dragon', 'Dark', 'Ghost', 'Psychic', 'Bug', 'Steel', 'Poison', 'Normal', 'Fighting', 'Flying']
+from enum import Enum
+class PokemonType(Enum):
+    FIRE= 'Fire'
+    WATER= 'Water'
+    GRASS= 'Grass'
+    ELECTRIC= 'Electric'
+    ICE= 'Ice'
+    GROUND= 'Ground'
+    ROCK= 'Rock'
+    FAIRY= 'Fairy'
+    DRAGON= 'Dragon'
+    DARK= 'Dark'
+    GHOST= 'Ghost'
+    PSYCHIC= 'Psychic'
+    BUG= 'Bug'
+    STEEL= 'Steel'
+    POISON= 'Poison'
+    NORMAL= 'Normal'
+    FIGHTING= 'Fighting'
+    FLYING= 'Flying'
+
 print(f'number of valid types:{len(valid_pokemon_type)}')
+weak_table = {PokemonType.BUG:[PokemonType.FIRE, PokemonType.FLYING,PokemonType.ROCK],
+              PokemonType.DARK:[PokemonType.BUG, PokemonType.FAIRY, PokemonType.ICE], 
+              PokemonType.DRAGON:[PokemonType.DRAGON, PokemonType.FAIRY, PokemonType.ICE],
+              PokemonType.ELECTRIC
+              PokemonType.FIRE:[PokemonType.GROUND, PokemonType.ROCK, PokemonType.WATER],
+              PokemonType.WATER:[PokemonType.ELECTRIC,PokemonType.GRASS], 
+              PokemonType.GRASS:['bug','fire',]}
